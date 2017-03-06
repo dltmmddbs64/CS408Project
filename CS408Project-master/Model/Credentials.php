8eae3eea633bf1fce6dbeed2466b589a9fe8c51e
TristinH <tristin95@yahoo.com>
2017-03-05 17:27:56 -0800

    Delete Credentials.php

--- CS408Project-master/Model/Credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Login data for the database. Use this file in all Models.
-
-$host = "localhost"; //Hostname, always "localhost"
-$user = "root"; //Username, always "root"
-$passwd = ""; //Password for the localhost, always ""
-$database = "facultystaffdb"; //Name of the database that is used
-?>
